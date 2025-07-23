99a256909e21da801a32d5b41f617cbffc849ce4
nishthapnt <nishthapnt@gmail.com>
2025-07-23 12:19:25 +0530

    all files added

---
+++ complete files/canteen block/app-files/data.js
@@ -0,0 +1,583 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-8",
+      "name": "8",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2717,
+      "initialViewParameters": {
+        "yaw": 1.263855526363514,
+        "pitch": -0.09498866634926628,
+        "fov": 1.3401592523986738
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.08549329856123755,
+          "pitch": -0.04370511386198217,
+          "rotation": 5.497787143782138,
+          "target": "2-1"
+        },
+        {
+          "yaw": 3.007882464510507,
+          "pitch": 0.09139676789757445,
+          "rotation": 0,
+          "target": "9-5"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.0919858705879744,
+          "pitch": -0.26317613121308625,
+          "title": "Canteen",
+          "text": "<br>"
+        },
+        {
+          "yaw": 2.1707900039559647,
+          "pitch": -0.10371097321763934,
+          "title": "Labs",
+          "text": "<br>"
+        }
+      ]
+    },
+    {
+      "id": "1-4",
+      "name": "4",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2681,
+      "initialViewParameters": {
+        "yaw": -2.824489852750837,
+        "pitch": -0.1995950476588213,
+        "fov": 1.3401592523986738
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.5923397353619295,
+          "pitch": 0.12121047135564211,
+          "rotation": 5.497787143782138,
+          "target": "4-3"
+        },
+        {
+          "yaw": 2.1141649771716153,
+          "pitch": 0.09187982867701905,
+          "rotation": 0.7853981633974483,
+          "target": "5-7"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.365646314526102,
+          "pitch": 0.07472425494724533,
+          "title": "Parking Area",
+          "text": "<br>"
+        },
+        {
+          "yaw": -2.8260920860084386,
+          "pitch": -0.19055601436914316,
+          "title": "Main Building",
+          "text": "<br>"
+        },
+        {
+          "yaw": -0.4383074868270782,
+          "pitch": -0.1908198647803374,
+          "title": "Basketball Court",
+          "text": "<br>"
+        }
+      ]
+    },
+    {
+      "id": "2-1",
+      "name": "1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2694,
+      "initialViewParameters": {
+        "yaw": -1.4755297052250196,
+        "pitch": -0.16351400093164514,
+        "fov": 1.3401592523986738
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.6024806054259564,
+          "pitch": 0.041338029797771014,
+          "rotation": 0,
+          "target": "3-2"
+        },
+        {
+          "yaw": 1.6049722604982462,
+          "pitch": 0.0651316158741011,
+          "rotation": 7.0685834705770345,
+          "target": "0-8"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-2",
+      "name": "2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3067.5,
+      "initialViewParameters": {
+        "yaw": -1.9571440227002164,
+        "pitch": -0.09169785850812673,
+        "fov": 1.3401592523986738
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.39422752205163825,
+          "pitch": 0.07553185258740136,
+          "rotation": 0,
+          "target": "2-1"
+        },
+        {
+          "yaw": -2.23052236269535,
+          "pitch": 0.048237078328599026,
+          "rotation": 0,
+          "target": "4-3"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.8541683652312484,
+          "pitch": 0.015089442876421444,
+          "title": "Football Ground",
+          "text": "<br>"
+        },
+        {
+          "yaw": -0.1517485875427269,
+          "pitch": -0.11671600345080968,
+          "title": "Bridge to girl's Hostel",
+          "text": "<br>"
+        }
+      ]
+    },
+    {
+      "id": "4-3",
+      "name": "3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2934,
+      "initialViewParameters": {
+        "yaw": -2.5858514316926033,
+        "pitch": 0.010800309015142062,
+        "fov": 1.3401592523986738
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.16599187550774985,
+          "pitch": 0.10402766985882472,
+          "rotation": 0.7853981633974483,
+          "target": "3-2"
+        },
+        {
+          "yaw": -2.6477427018920547,
+          "pitch": 0.04005734835357799,
+          "rotation": 0,
+          "target": "1-4"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.889166661919135,
+          "pitch": -0.06523741741936284,
+          "title": "Voleyball Court",
+          "text": "<br>"
+        }
+      ]
+    },
+    {
+      "id": "5-7",
+      "name": "7",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2674,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.9492026907885496,
+          "pitch": 0.03479016583910166,
+          "rotation": 7.0685834705770345,
+          "target": "1-4"
+        },
+        {
+          "yaw": 0.274005820155077,
+          "pitch": -0.012172241483941804,
+          "rotation": 0,
+          "target": "7-10"
+        },
+        {
+          "yaw": -2.0071767678959738,
+          "pitch": 0.018265677292774285,
+          "rotation": 0.7853981633974483,
+          "target": "6-6"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.0267955343683255,
+          "pitch": -0.05376751433910343,
+          "title": "Parking Area",
+          "text": "<br>"
+        }
+      ]
+    },
+    {
+      "id": "6-6",
+      "name": "6",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3698.5,
+      "initialViewParameters": {
+        "yaw": 0.4185228308353821,
+        "pitch": -0.22934271358240466,
+        "fov": 1.3401592523986738
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.102604281414335,
+          "pitch": -0.07365893176697469,
+          "rotation": 5.497787143782138,
+          "target": "5-7"
+        },
+        {
+          "yaw": 0.21613882396525597,
+          "pitch": -0.03999938423395655,
+          "rotation": 5.497787143782138,
+          "target": "0-8"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.9878581558558857,
+          "pitch": -0.1942662525690544,
+          "title": "Main Stage",
+          "text": "<br>"
+        },
+        {
+          "yaw": 1.6161004210605503,
+          "pitch": -0.14023020569416644,
+          "title": "Water Reservoir",
+          "text": "<br>"
+        }
+      ]
+    },
+    {
+      "id": "7-10",
+      "name": "10",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2647,
+      "initialViewParameters": {
+        "yaw": -2.415071228342754,
+        "pitch": -0.2413935093013606,
+        "fov": 1.3401592523986738
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.016091377968113,
+          "pitch": 0.0891826937956619,
+          "rotation": 0,
+          "target": "5-7"
+        },
+        {
+          "yaw": 2.0143265434462787,
+          "pitch": 0.17130595087344247,
+          "rotation": 4.71238898038469,
+          "target": "8-9"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.740649257693713,
+          "pitch": -0.04841829921895169,
+          "title": "Old Building Entry Gate",
+          "text": "<br>"
+        }
+      ]
+    },
+    {
+      "id": "8-9",
+      "name": "9",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2710.5,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.9246407479253484,
+          "pitch": 0.06751778365823391,
+          "rotation": 13.351768777756625,
+          "target": "7-10"
+        },
+        {
+          "yaw": 0.12440995821689782,
+          "pitch": 0.10540578681779245,
+          "rotation": 0,
+          "target": "9-5"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-5",
+      "name": "5",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2633.5,
+      "initialViewParameters": {
+        "yaw": -1.4471592371008093,
+        "pitch": -0.022032585538028115,
+        "fov": 1.3401592523986738
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.715813227754185,
+          "pitch": 0.18606904843020544,
+          "rotation": 0.7853981633974483,
+          "target": "8-9"
+        },
+        {
+          "yaw": -0.7118738892754557,
+          "pitch": 0.05699830758678104,
+          "rotation": 0,
+          "target": "0-8"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.28338063122823,
+          "pitch": 0.06803569060160797,
+          "title": "Fish reservior",
+          "text": "<br>"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
---
+++ virtual_frontend/resources/js/script.js
@@ -0,0 +1,7 @@
+// JS using Typed.js (library)
+new Typed(".typed-text", {
+  strings: ["Explore the campus", "Join the experience", "Start your journey"],
+  typeSpeed: 60,
+  backSpeed: 40,
+  loop: true
+});
